70495f6933e7d41655d03c4a0544886a6d373256
Glitch (daily-time-tracker) <none>
2021-07-01 12:40:18 +0000

    🌥🐁 Checkpoint ./sw.js:32914662/1073

--- sw.js
+++ sw.js
@@ -21,11 +21,40 @@ self.addEventListener('install', function(e) {
 
 //Serve cached content when offline
 self.addEventListener('fetch', function(e) {
-  e.respondWith(
-    caches.match(e.request).then(function(response) {
-      return response || fetch(e.request);
-    })
-  );
+  e.respondWith(fromCache(e.request));
 
-  
+  e.waitUntil(
+    update(e.request)
+    .then(refresh)
+  );
 });
+
+function fromCache(request) {
+  return caches.open(cacheName).then(function (cache) {
+    return cache.match(request);
+  });
+}
+
+function update(request) {
+  return caches.open(cacheName).then(function (cache) {
+    return fetch(request).then(function (response) {
+      return cache.put(request, response.clone()).then(function () {
+        return response;
+      });
+    });
+  });
+}
+
+function refresh(response) {
+  return self.clients.matchAll().then(function (clients) {
+    clients.forEach(function (client) {
+      var message = {
+        type: 'refresh',
+        url: response.url,
+        eTag: response.headers.get('ETag')
+      };
+
+      client.postMessage(JSON.stringify(message));
+    });
+  });
+}
